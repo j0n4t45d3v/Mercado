05005ba2f7fd09c4e91e1bb580506ae1dc88ac0a
P1ul0 <paulocesar147852@gmail.com>
2022-11-16 19:29:17 -0300

    addFuncionario

--- src/entidade/dados/RepositorioFuncionarioVetor.java
+++ src/entidade/dados/RepositorioFuncionarioVetor.java
@@ -1,11 +1,28 @@
 package entidade.dados;
 
+import entidade.cliente.Cliente;
 import entidade.funcionario.Funcionario;
 
 public class RepositorioFuncionarioVetor implements InterfaceFuncionario{
+    private Funcionario [] vetor;
+    private int indice;
+
+    public RepositorioFuncionarioVetor(int t) {
+      this.vetor = new Funcionario[t];
+      this.indice = 0;
+    }
+
     @Override
     public void add(Funcionario funcionario) {
-
+        if (this.vetor.length == indice){
+            Funcionario[] aux = new Funcionario[indice * 2];
+            for (int i = 0 ; i < indice ; i++){
+                aux[i] = this.vetor[i];
+            }
+            this.vetor = aux;
+        }
+        this.vetor[indice] = funcionario;
+        indice++;
     }
 
     @Override
@@ -26,5 +43,21 @@ public class RepositorioFuncionarioVetor implements InterfaceFuncionario{
     @Override
     public Funcionario buscarFuncionario(String nome) {
         return null;
+    }
+
+    public Funcionario[] getVetor() {
+        return vetor;
+    }
+
+    public void setVetor(Funcionario[] vetor) {
+        this.vetor = vetor;
+    }
+
+    public int getIndice() {
+        return indice;
+    }
+
+    public void setIndice(int indice) {
+        this.indice = indice;
     }
 }
