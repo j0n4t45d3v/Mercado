c51d7253490c60a745213ddf57fc1acd8afff4e1
P1ul0 <paulocesar147852@gmail.com>
2022-11-16 20:15:18 -0300

    ajustes

--- src/entidade/dados/RepositorioProdutoVetor.java
+++ src/entidade/dados/RepositorioProdutoVetor.java
@@ -4,38 +4,67 @@ import entidade.produto.Produto;
 
 public class RepositorioProdutoVetor implements InterfaceProduto {
 
-    private Produto[] produto;
+    private Produto[] vetor;
 
     private int indice;
 
     public RepositorioProdutoVetor(int t) {
-        this.produto = new Produto[t];
+        this.vetor = new Produto[t];
         this.indice = 0;
     }
 
     @Override
     public void add(Produto produto) {
-
-
+    if (this.vetor.length == indice){
+        Produto[] aux = new Produto[indice * 2];
+        for (int i = 0 ; i <indice; i++){
+            aux[i] = vetor[i];
+        }
+        this.vetor = aux;
+    }
+    this.vetor[indice]= produto;
+    indice++;
     }
 
     @Override
     public void remover(Produto produto) {
+        for (int i = 0 ; i < this.vetor.length; i++){
+            if (this.vetor[i].getId().equals(produto.getId())){
+                this.vetor[i] = null;
+            }
+        }
 
     }
 
     @Override
     public void atualizar(Produto produto, String id) {
-
+        for (int i = 0 ; i < this.vetor.length ; i++){
+            if (this.vetor[i].getId().equals(id)){
+                this.vetor[i] = produto;
+            }
+        }
     }
 
     @Override
     public boolean existeProduto(String id) {
-        return false;
+        boolean produtoEncontrado = false;
+        for (Produto produto : this.vetor) {
+            if (produto.getId().equals(id)) {
+                produtoEncontrado = true;
+                break;
+            }
+        }
+        return produtoEncontrado;
     }
 
     @Override
     public Produto buscarProduto(String codigoDeBarra) {
-        return null;
+        Produto existe = null;
+        for (Produto produto : this.vetor) {
+            if (produto.getId().equals(codigoDeBarra)){
+                existe = produto;
+            }
+        }
+        return existe;
     }
 }
