691d9e828c934a8f108d9170351514ad3ca3f8ec
Leratago <111026043+Leratago@users.noreply.github.com>
2022-11-23 21:25:49 -0300

    ajuste lista

--- src/entidade/dados/RepositorioFuncionarioLista.java
+++ src/entidade/dados/RepositorioFuncionarioLista.java
@@ -31,7 +31,27 @@ public class RepositorioFuncionarioLista implements InterfaceFuncionario{
     @Override
     public void remover(Funcionario funcionario) {
         Node<Funcionario> no = new Node<>(funcionario);
+        no = this.inicio;
+        while (no != null && no.getValue() != funcionario) {
+            no = no.getProx();
+        }
+        if (no != null && no.equals(this.inicio) && no.equals(this.fim)){
+            this.inicio.setValue(null);
+            this.fim.setValue(null);
+            this.fim.setAnt(null);
+        } else if (this.inicio != null && this.inicio.equals(no)) {
+            this.inicio = no.getProx();
+            this.inicio.setAnt(null);
+        } else if (this.fim.equals(no)) {
+            this.fim = no.getAnt();
+            this.fim.setProx(null);
+        }else {
+            no.getAnt().setProx(no.getProx());
+            no.getProx().setAnt(no.getAnt());
+        }
+        tamanho--;
     }
+
 
     @Override
     public void atualizar(Funcionario funcionario, String cpf) {
